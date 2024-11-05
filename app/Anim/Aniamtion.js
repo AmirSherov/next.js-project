d67d9725ac498d300fb33e2f3c7b8d665384b2e4
Mirzo1664 <wner89485@gmail.com>
2024-11-06 01:38:30 +0500

    animation

---
+++ app/Anim/Aniamtion.js
@@ -0,0 +1,13 @@
+import "./style.scss"
+export default function Anim(){
+    return (
+        <>
+            <section>
+                <div className="wave wave1"></div>
+                <div className="wave wave2"></div>
+                <div className="wave wave3"></div>
+                <div className="wave wave4"></div>
+            </section>
+        </>
+    )
+}
